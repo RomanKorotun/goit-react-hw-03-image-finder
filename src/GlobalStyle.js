f629be08c9764d0a87ff69df96bbba1f3ba1c183
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-10-12 22:37:06 +0300

    first commit

---
+++ src/GlobalStyle.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+
+ul,
+ol {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+`;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+export class App extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
